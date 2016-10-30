3e75f9aa0a7e951e62404b703e4babc704a58e1a
Jenn Tustin <jenn.tustin@gmail.com>
2016-10-30 09:11:00 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+
   root 'static_pages#home'
+  # get 'static_pages/home'
 
-  get 'static_pages/home'
+  # get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
 
-  get 'static_pages/help'
+  # get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
-  get 'static_pages/about'
+  # get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
 
-  get 'static_pages/contact'
   #root 'application#hello'
 end
+
+# Page	URL	Named route
+# Home	/	root_path
+# About	/about	about_path
+# Help	/help	help_path
+# Contact	/contact	contact_path
+# Sign up	/signup	signup_path
+# Log in	/login	login_path
---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+# require 'test_helper'
+#
+# class ApplicationHelperTest < ActionView::TestCase
+#   test "full title helper" do
+#     assert_equal full_title, full_title(yield(:title))
+#     assert_equal full_title("Help"), full_title(yield("Help"))
+#   end
+# end
+#  FILL in ???
+# also test_helper.rb in correct place??
